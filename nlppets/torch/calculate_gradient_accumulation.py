790ebb144fcc2709533d7f1e106bc471e7e191f6
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-04-13 17:12:05 +0800

    :sparkles: allow custom device count in calculate gradient accumulation

--- nlppets/torch/calculate_gradient_accumulation.py
+++ nlppets/torch/calculate_gradient_accumulation.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 import torch.distributed
 
 from .count_cuda_devices import count_cuda_devices
 
 
 def calculate_gradient_accumulation(
-    batch_size_per_device: int, target_batch_size: int
+    batch_size_per_device: int,
+    target_batch_size: int,
+    device_count: Optional[int] = None,
 ) -> int:
     """Calculate the number of gradient accumulation steps for a given batch size.
 
@@ -17,18 +21,21 @@ def calculate_gradient_accumulation(
     Returns:
         The number of gradient accumulation steps.
     """
-    if torch.distributed.is_initialized():
+    if device_count is not None:
+        gpu_cpu_count = device_count
+    elif torch.distributed.is_initialized():
         gpu_cpu_count = torch.distributed.get_world_size()
-        if gpu_cpu_count <= 0:
-            raise ValueError(
-                f"Torch is running in distributed mode, but the world size is {gpu_cpu_count}."
-            )
     else:
         gpu_cpu_count = count_cuda_devices() or 1
+
+    if gpu_cpu_count <= 0:
+        raise ValueError(f"Invalid device count {gpu_cpu_count}.")
+
     batch = batch_size_per_device * gpu_cpu_count
-    if target_batch_size % batch:
+    step, remain = divmod(target_batch_size, batch)
+    if remain != 0:
         raise ValueError(
             f"The target batch size {target_batch_size} is not a multiple of "
             f"{batch_size_per_device} * {gpu_cpu_count}"
         )
-    return target_batch_size // batch
+    return step
